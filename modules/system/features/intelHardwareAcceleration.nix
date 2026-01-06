dd94bd803d385c355fb4685ca1d76f6794f3b8be
Ryan Swift <ryanaswift7@gmail.com>
2026-01-05 20:37:41 -0800

    trying different intel iGPU driver

--- modules/system/features/intelHardwareAcceleration.nix
+++ modules/system/features/intelHardwareAcceleration.nix
@@ -7,9 +7,10 @@
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        intel-media-driver
+        intel-media-driver  # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+        intel-vaapi-driver  # For older processors. LIBVA_DRIVER_NAME=i965
       ];
     };
-    environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+    environment.sessionVariables = { LIBVA_DRIVER_NAME = "i965"; };
   };
 }
