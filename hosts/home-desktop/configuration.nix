c1224fab3d0750aa2da889735f5b3e21d29de38a
Ryan Swift <ryanaswift7@gmail.com>
2026-01-05 16:36:45 -0800

    make sure all default.nix are imports

--- hosts/home-desktop/configuration.nix
+++ hosts/home-desktop/configuration.nix
@@ -1,61 +1,27 @@
-
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  systemSuites.workstation = true;
+  systemFeatures.nvidia.enable = true;
+  systemFeatures.intelHardwareAcceleration.enable = true;
 
-    # nvidia
-    hardware.graphics.enable = true;
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia = {
-      open = true;
-      modesetting.enable = true;
-      nvidiaSettings = true;
-      powerManagement.enable = true;
-    };
+  # to directly add a new package
+  environment.systemPackages = with pkgs; [
+
+  ];
+
+  home-manager.users."${config.userSettings.username}" = { ... }: {
+    homeSuites.workstation = true;
+    homeFeatures.school.enable = true;
+  };
 
   specialisation."LTS".configuration = {
-    environment.etc."specialisation".text = "LTS";
     system.nixos.tags = [ "LTS" ];
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   };
 
   specialisation."GAMING".configuration = {
-    environment.etc."specialisation".text = "GAMING";
     system.nixos.tags = [ "GAMING" ];
-    boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
-
-    boot.kernelParams = [ 
-          "split_lock_mitigate=0" # Improves performance in some games by disabling a CPU mitigation
-    ];
-
-    boot.kernel.sysctl = {
-      "vm.max_map_count" = 2147483642; # Essential for heavy games like Star Citizen or Elden Ring
-    };
-
-    programs.gamemode.enable = true;
-    programs.gamescope.enable = true; # Micro-compositor for better upscaling/latency
-
-    programs.steam = {
-      enable = true;
-      # remotePlay.openFirewall = true;
-      # dedicatedServer.openFirewall = true;
-    };
-       
-    environment.systemPackages = with pkgs; [
-      mangohud    # On-screen FPS and performance overlay
-      protonup-qt # Easy way to install GE-Proton versions
-      lutris      # Launcher for non-Steam games
-      heroic
-      wineWowPackages.waylandFull
-      gamescope-wsi # HDR won't work without this
-    ];
-
-     powerManagement.cpuFreqGovernor = "performance";
+    systemSuites.gaming = lib.mkForce true;
   };
-
-
 }
--- hosts/home-desktop/default.nix
+++ hosts/home-desktop/default.nix
@@ -1,45 +1,7 @@
-{ config, pkgs, lib, inputs, ... }:
-
-# let
-#   user = {
-#     username = "ryan";
-#     homeDirectory = "/home/ryan";
-#     dotfileDirectory = "/home/ryan/nix-dots/dotfiles";
-#     fullName = "Ryan Swift";
-#     email = "ryanaswift7@gmail.com";
-#     hostName = "home-desktop";
-#     systemStateVersion = "25.05";
-#     homeStateVersion = "25.05";
-#   };
-# in
+{ ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
-
-  # this validates the user in the let block
-  # userSettings = user;
-
-  systemSuites.workstation = true;
-  systemFeatures.nvidia.enable = true;
-  systemFeatures.intelHardwareAcceleration.enable = true;
-
-  # to directly add a new package
-  environment.systemPackages = with pkgs; [
-
+  imports = [ 
+    ./hardware-configuration.nix 
+    ./configuration.nix
   ];
-
-
-  home-manager.users."${config.userSettings.username}" = { ... }: {
-    homeSuites.workstation = true;
-    homeFeatures.school.enable = true;
-  };
-
-  specialisation."LTS".configuration = {
-    system.nixos.tags = [ "LTS" ];
-    boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-  };
-
-  specialisation."GAMING".configuration = {
-    system.nixos.tags = [ "GAMING" ];
-    systemSuites.gaming = lib.mkForce true;
-  };
 }
